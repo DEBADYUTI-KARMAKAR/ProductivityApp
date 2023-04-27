dbe866dc188a392e30faf22dfcf76a66855bd0a3
Debadyuti <debadyuti.karmakar@gmail.com>
2023-04-27 11:55:30 +0530

    Configuring Spring Security to Get H2 console Working

--- ProductivityApp/src/main/java/com/springboot/webapp/productivityapp/security/SpringSecurityConfigaration.java
+++ ProductivityApp/src/main/java/com/springboot/webapp/productivityapp/security/SpringSecurityConfigaration.java
@@ -1,14 +1,18 @@
 package com.springboot.webapp.productivityapp.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfigaration {
@@ -46,5 +50,28 @@ public class SpringSecurityConfigaration {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}  
+	
+	
+	
+	//All URLS are protected
+	// A login form is shown for unauthorized requests
+	// CRCF disable
+	// Frames
+	
+	@Bean
+	public SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated());
+
+
+		http.formLogin(withDefaults());
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		
+		return http.build();
 	}
+	
 }
